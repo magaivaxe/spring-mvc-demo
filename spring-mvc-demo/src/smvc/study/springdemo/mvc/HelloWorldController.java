7c356d4e0b81df9c117e648a836598648a5927a6
Hudon <Hudon@Portier-Evolo2>
2018-09-24 02:45:50 -0400

    Form exemple

---
+++ spring-mvc-demo/src/smvc/study/springdemo/mvc/HelloWorldController.java
@@ -0,0 +1,21 @@
+package smvc.study.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	
+	// need a controller method to show initial html form
+	
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "helloworld-form";
+	}
+	// need a controller method to process the html form
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
+	
+}
