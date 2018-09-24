1342efb28d6f6a960c0eef08b4303e5ba02b8f58
Hudon <Hudon@Portier-Evolo2>
2018-09-24 01:33:23 -0400

    First demo page

---
+++ spring-mvc-demo/src/smvc/study/springdemo/mvc/HomeController.java
@@ -0,0 +1,13 @@
+package smvc.study.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	//
+	@RequestMapping("/")
+	public String showPage() {
+		return "main-menu";
+	}
+}
