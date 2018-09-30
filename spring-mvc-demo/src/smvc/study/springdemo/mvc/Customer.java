4e96c45b995bfea15886f227b0f2ee5d66fb5656
Hudon <Hudon@Portier-Evolo2>
2018-09-30 04:39:17 -0400

    Errors by file properties

--- spring-mvc-demo/src/smvc/study/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/smvc/study/springdemo/mvc/Customer.java
@@ -13,9 +13,13 @@ public class Customer {
 	// Annotations rules to validation
 	@NotNull(message="is required") @Size(min=1, message="is required")
 	private String lastName;
+	
+	// The message from not null will appear only with Integer class because with primitive integer
+	// fails to parse to string
+	@NotNull(message="passes Required")
 	@Min(value=0, message="greater than or equal to zero")
 	@Max(value=10, message="less than or equal to 10")
-	private int freePasses;
+	private Integer freePasses;
 	// this pattern means that you can add a-z or A-Z or 0-9 with length 5.
 	// if you want only digits it just write 0-9 with the desire length.
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
@@ -33,10 +37,10 @@ public class Customer {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 	public String getPostalCode() {
