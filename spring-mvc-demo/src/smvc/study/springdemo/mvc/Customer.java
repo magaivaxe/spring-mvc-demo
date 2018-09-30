0bb85ca956c6702ae7081f2b46476517bbeee29b
Hudon <Hudon@Portier-Evolo2>
2018-09-30 04:07:02 -0400

    Postal code with regex

--- spring-mvc-demo/src/smvc/study/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/smvc/study/springdemo/mvc/Customer.java
@@ -1,15 +1,25 @@
 package smvc.study.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
 	// Fields
 	private String firstName;
 	
-	// Annotations to validation
+	// Annotations rules to validation
 	@NotNull(message="is required") @Size(min=1, message="is required")
 	private String lastName;
+	@Min(value=0, message="greater than or equal to zero")
+	@Max(value=10, message="less than or equal to 10")
+	private int freePasses;
+	// this pattern means that you can add a-z or A-Z or 0-9 with length 5.
+	// if you want only digits it just write 0-9 with the desire length.
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
+	private String postalCode;
 
 	public String getFirstName() {
 		return firstName;
@@ -22,6 +32,18 @@ public class Customer {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	public int getFreePasses() {
+		return freePasses;
+	}
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 	
 }
